bdd64e0a931cd1d808f01a17c35633cec458dbd5
Kevin Lee <lining@luojilab.com>
2017-07-07 00:19:57 +0800

    add some examples

---
+++ variable_types/demo2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// function declaration
+int func();
+
+int main() {
+    // function call
+    int i = func();
+    cout << i << endl;
+}
+
+int func() {
+    return 0;
+}
