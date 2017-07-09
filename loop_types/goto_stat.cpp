fd50dc049300afeb23b88d2ad3bf1a0509a8d3c2
Kevin Lee <lining@luojilab.com>
2017-07-09 13:42:09 +0800

    add goto and infinite loop examples.

---
+++ loop_types/goto_stat.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // local variable declaration
+    int a = 10;
+
+    LOOP: do {
+        if (a == 15) {
+            a++;
+            goto LOOP;
+        }
+    
+        cout << "value of a: " << a << endl;
+        a++;
+    } while (a < 20);
+
+    return 0;
+}
