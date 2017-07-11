db9c56134dcfb3af70cfe9548320b807de331bfb
Kevin Lee <lining@luojilab.com>
2017-07-11 23:35:09 +0800

    add pass by reference and default value example

--- functions/swap2.cpp
+++ functions/swap2.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 // function declaration
-void swap(int *x, int *y);
+void swap(int &x, int &y);
 
 // function declaration to swap the values
-void swap(int *x, int *y) {
+void swap(int &x, int &y) {
     int temp;
-    temp = *x; /* save the value of x */
-    *x = *y;    /* put y into x */
-    *y = temp; /* put x into y */
+    temp = x; /* save the value of x */
+    x = y;    /* put y into x */
+    y = temp; /* put x into y */
 
     return;
 }
@@ -26,7 +26,7 @@ int main() {
      * &a indicates pointer to a ie. address of variable a and
      * &b indicates pointer to b ie. address of variable b.
      */
-    swap(&a, &b);
+    swap(a, b);
 
     cout << "after swap, value of a: " << a << endl;
     cout << "after swap, value of b: " << b << endl;
